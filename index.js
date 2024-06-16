023057c88bbbc60cbf1ba543a1fb767e0afadf72
Jay Sanghani <sanghanijay6353@gmail.com>
2024-06-16 09:26:00 +0530

    add templete

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
-const { generateEmailTemplate, generateInquiryEmailTemplate } = require("./emailTemplates");
+const { generateEmailTemplate, generateInquiryEmailTemplate } = require("./emailtemplets.js");
 
 const app = express();
 const port = process.env.PORT || 3001;
